2c4760d1b11d022369cb4d46f5d50c2a86c9e13b
Arifin <qccikande1.official@gmail.com>
2026-06-02 14:56:35 +0700

    update file migrate micings

---
+++ database/migrations/2026_06_02_145600_add_waktu_start_end_to_mincings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('mincings', function (Blueprint $table) {
+            // PREMIX
+            $table->time('waktu_mixing_premix_start')
+                ->nullable()
+                ->after('waktu_mixing_premix');
+
+            $table->time('waktu_mixing_premix_end')
+                ->nullable()
+                ->after('waktu_mixing_premix_start');
+
+            // BOWL CUTTER
+            $table->time('waktu_bowl_cutter_start')
+                ->nullable()
+                ->after('waktu_bowl_cutter');
+
+            $table->time('waktu_bowl_cutter_end')
+                ->nullable()
+                ->after('waktu_bowl_cutter_start');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('mincings', function (Blueprint $table) {
+            $table->dropColumn([
+                'waktu_mixing_premix_start',
+                'waktu_mixing_premix_end',
+                'waktu_bowl_cutter_start',
+                'waktu_bowl_cutter_end',
+            ]);
+        });
+    }
+};
