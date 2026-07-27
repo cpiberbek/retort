af3ef841ab813e2adb6b65e50e7af891a2db1a1e
Bagus <fraber.above@gmail.com>
2026-07-27 10:39:27 +0700

    Convert Laporan Pengambilan Sampel PDF: redesign pdf based off template excel, persetujuan SPV: Disetujui oleh "$nama_spv"

--- resources/views/reports/pengambilan-sampel.blade.php
+++ resources/views/reports/pengambilan-sampel.blade.php
@@ -25,21 +25,34 @@
             vertical-align: middle;
             font-weight: bold;
         }
+        body,
+        table,
+        tr,
+        td,
+        th {
+            font-family: times;
+        }
     </style>
 </head>
 
 <body>
 
 {{-- HEADER --}}
-<table width="100%">
-    <tr>
-        <td class="small" width="40%">
-            PT Charoen Pokphand Indonesia<br>
-            Food Division
-        </td>
-        
-    </tr>
-</table>
+<div style="margin-left:-30px;">
+    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td width="55">
+                <img src="{{ public_path('assets/img/Logo CPI.png') }}" width="50">
+            </td>
+            <td>
+                <span style="font-size:14pt;"><b>PT Charoen </b></span><br>
+                <span style="font-size:14pt;"><b>Pokphand Indonesia</b></span><br>
+                <span style="font-size:14pt;"><b>Food Division</b></span>
+            </td>
+        </tr>
+    </table>
+</div>
+
 <h2 class="title">PENGAMBILAN SAMPEL</h2>
 <br>
 <br>
@@ -47,10 +60,10 @@
 {{-- TABEL UTAMA --}}
 <table width="100%" class="tbl-main small">
     <tr>
-        <th class="center">Tgl Pengambilan</th>
+        <th class="center">Tgl. Pengambilan</th>
         <th class="center">Jenis Sampel</th>
-        <th class="center">Nama Varian</th>
-        <th class="center">Kode Batch</th>
+        <th class="center">Nama Produk</th>
+        <th class="center">Kode Produk</th>
         <th class="center">Keterangan</th>
         <th class="center">Paraf SPV</th>
     </tr>
@@ -60,9 +73,11 @@
         <td>{{ \Carbon\Carbon::parse($item->date)->format('d-m-Y') }}</td>
         <td>{{ $item->jenis_sampel }}</td>
         <td>{{ $item->nama_produk }}</td>
-        <td>{{ $item->kode_produksi }}</td>
+        <td>{{ optional(\App\Models\Mincing::where('uuid', $item->kode_produksi)->first())->kode_produksi }}</td>
         <td>{{ $item->keterangan }}</td>
-        <td></td>
+        <td>
+            {{ $item->nama_spv ? 'Disetujui oleh '.$item->nama_spv : 'Belum disetujui' }}
+        </td>
     </tr>
     @endforeach
 
@@ -80,7 +95,14 @@
     @endif
 </table>
 
-<div style="text-align:right; font-size:8px;font-style:italic">QT 30 / 00</div>
+<table width="100%">
+    <tr>
+        <td width="75%"></td>
+        <td width="25%" align="right" style="font-style: italic;">
+            {{ $noDokumen }}
+        </td>
+    </tr>
+</table>
 
 </body>
 </html>
