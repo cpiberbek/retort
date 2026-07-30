362bb58f422ee9976b909d50662c14c56df6505a
Bagus <fraber.above@gmail.com>
2026-07-30 11:19:19 +0700

    Fix Convert Data Lot Wire PDF: mengikuti format excel

--- resources/views/reports/wire.blade.php
+++ resources/views/reports/wire.blade.php
@@ -92,109 +92,236 @@
 
 <body>
 
-@php
-$grids = [];
+    @php
+    $grids = [];
 
-foreach ($items as $item) {
-    $dataWire = json_decode($item->data_wire, true);
+    foreach ($items as $item) {
+        $dataWire = json_decode($item->data_wire, true);
 
-    if (!is_array($dataWire)) {
-        continue;
+        if (!is_array($dataWire)) {
+            continue;
+        }
+
+        foreach ($dataWire as $mesin) {
+
+            foreach ($mesin['detail'] ?? [] as $dtl) {
+
+                $grids[] = [
+                    'tanggal'  => $item->date,
+                    'shift'    => $item->shift,
+                    'supplier' => $item->nama_supplier ?? '-',
+                    'produk'   => $item->nama_produk ?? '-',
+                    'catatan'  => $item->catatan ?? '-',
+                    'status'   => $item->status_spv ?? '-',
+                    'mesin'    => $mesin['mesin'] ?? '-',
+                    'detail'   => [$dtl],
+                ];
+
+            }
+
+        }
     }
 
-    foreach ($dataWire as $mesin) {
-        $grids[] = [
-            'tanggal'  => $item->date,
-            'shift'    => $item->shift,
-            'supplier' => $item->nama_supplier ?? '-',
-            'produk'   => $item->nama_produk ?? '-',
-            'catatan'  => $item->catatan ?? '-',
-            'status'   => $item->status_spv ?? '-',
-            'mesin'    => $mesin['mesin'] ?? '-',
-            'detail'   => $mesin['detail'] ?? [],
-        ];
-    }
-}
+    //in case usernya meminta 1 mesin many lot
+    // foreach ($items as $item) {
+    //     $dataWire = json_decode($item->data_wire, true);
 
-$totalPage = ceil(count($grids) / 9);
-@endphp
+    //     if (!is_array($dataWire)) {
+    //         continue;
+    //     }
 
-@foreach(array_chunk($grids, 9) as $pageIndex => $rows)
+    //     foreach ($dataWire as $mesin) {
+    //         $grids[] = [
+    //             'tanggal'  => $item->date,
+    //             'shift'    => $item->shift,
+    //             'supplier' => $item->nama_supplier ?? '-',
+    //             'produk'   => $item->nama_produk ?? '-',
+    //             'catatan'  => $item->catatan ?? '-',
+    //             'status'   => $item->status_spv ?? '-',
+    //             'mesin'    => $mesin['mesin'] ?? '-',
+    //             'detail'   => $mesin['detail'] ?? [],
+    //         ];
+    //     }
+    // }
 
-<table width="100%" style="border-collapse:collapse;border:1px solid #000;">
-    <tr>
-        <td width="25%" style="border:1px solid #000;text-align:center;padding:5px;">
-            <img src="{{ public_path('assets/img/Logofd.png') }}" width="50">
-        </td>
+    $totalPage = ceil(count($grids) / 9);
+    @endphp
 
-        <td width="50%" style="border:1px solid #000;text-align:center;font-size:18px;padding:5px;">
-            <b>FORM</b><br>
-            <b>VERIFIKASI MAGNET TRAP</b>
-        </td>
+    @foreach(array_chunk($grids, 9) as $pageIndex => $rows)
 
-        <td width="25%" style="border:1px solid #000;font-size:10px;padding:0;">
-            <table width="100%" style="border-collapse:collapse;">
+    <table width="100%" style="border-collapse:collapse;border:1px solid #000;">
+        <tr>
+            <td width="25%" style="border:1px solid #000;text-align:center;padding:5px;">
+                <img src="{{ public_path('assets/img/Logofd.png') }}" width="50">
+            </td>
+
+            <td width="50%" style="border:1px solid #000;text-align:center;font-size:18px;padding:5px;">
+                <b>FORM</b><br>
+                <b>VERIFIKASI MAGNET TRAP</b>
+            </td>
+
+            <td width="25%" style="border:1px solid #000;font-size:10px;padding:0;">
+                <table width="100%" style="border-collapse:collapse;">
+                    <tr>
+                        <td width="45%" style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
+                            No. Dokumen
+                        </td>
+                        <td style="border-bottom:1px solid #000;padding:3px;">
+                            : {{ $noDokumen }}
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
+                            Revisi
+                        </td>
+                        <td style="border-bottom:1px solid #000;padding:3px;">
+                            : {{ (int) substr(strrchr($noDokumen, '/'), 1) }}
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
+                            Tanggal Efektif
+                        </td>
+                        <td style="border-bottom:1px solid #000;padding:3px;">
+                            : 18-12-2023
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="border-right:1px solid #000;padding:3px;">
+                            Halaman
+                        </td>
+                        <td style="padding:3px;">
+                            : {{ $pageIndex + 1 }} dari {{ $totalPage }}
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    <table width="100%" cellspacing="0" cellpadding="3" border="1">
+        <tr>
+            <td width="50%">
+                <b>Hari / Tgl</b> :
+                {{ $request->date ? \Carbon\Carbon::parse($request->date)->format('d-m-Y') : 'SEMUA' }}
+            </td>
+
+            <td width="50%">
+                <b>Supplier Wire</b> :
+                {{ count($grids) ? $grids[0]['supplier'] : '-' }}
+            </td>
+        </tr>
+    </table>
+
+    <br>
+
+    <table width="100%" border="0" cellspacing="2" cellpadding="0">
+
+        @foreach(array_chunk($rows, 3) as $row)
+
+    {{-- //in case usernya minta data aneh2 ini lebih lengkap--}}
+    {{-- <tr>
+
+        @foreach($row as $grid)
+
+        <td width="33.33%" valign="top">
+
+            <table width="100%" border="1" cellspacing="0" cellpadding="3">
+
                 <tr>
-                    <td width="45%" style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
-                        No. Dokumen
-                    </td>
-                    <td style="border-bottom:1px solid #000;padding:3px;">
-                        : {{ $noDokumen }}
+                    <td colspan="2" align="center">
+                        <b>MESIN : {{ $grid['mesin'] }}</b>
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
-                        Revisi
+                    <td width="35%">
+                        Tanggal
                     </td>
-                    <td style="border-bottom:1px solid #000;padding:3px;">
-                        : {{ (int) substr(strrchr($noDokumen, '/'), 1) }}
-                    </td>
-                </tr>
-
-                <tr>
-                    <td style="border-right:1px solid #000;border-bottom:1px solid #000;padding:3px;">
-                        Tanggal Efektif
-                    </td>
-                    <td style="border-bottom:1px solid #000;padding:3px;">
-                        : 18-12-2023
+                    <td width="65%">
+                        {{ \Carbon\Carbon::parse($grid['tanggal'])->format('d-m-Y') }}
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="border-right:1px solid #000;padding:3px;">
-                        Halaman
+                    <td>
+                        Shift
                     </td>
-                    <td style="padding:3px;">
-                        : {{ $pageIndex + 1 }} dari {{ $totalPage }}
+                    <td>
+                        {{ $grid['shift'] }}
                     </td>
                 </tr>
+
+              
+                <tr>
+                    <td>
+                        Produk
+                    </td>
+                    <td>
+                        {{ $grid['produk'] }}
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <b>Detail Wire</b>
+                    </td>
+                </tr>
+
+                @if(!empty($grid['detail']) && is_array($grid['detail']))
+
+                    @foreach($grid['detail'] as $dtl)
+
+                    <tr>
+                        <td width="35%">
+                            <strong>Lot:</strong> 
+                        </td>
+
+                        <td width="65%">
+                        <strong>{{ $dtl['no_lot'] ?? '' }}</strong>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td width="35%">
+                            Start - End:
+                        </td>
+
+                        <td width="65%">
+                            {{ $dtl['start'] ?? '?' }} - {{ $dtl['end'] ?? '?' }}
+                        </td>
+                    </tr>
+
+                    @endforeach
+
+                @endif
+
+                <tr>
+                    <td>
+                        Catatan
+                    </td>
+
+                    <td>
+                        {{ $grid['catatan'] }}
+                    </td>
+                </tr>
+
             </table>
-        </td>
-    </tr>
-</table>
 
-<table width="100%" cellspacing="0" cellpadding="3" border="1">
+        </td>
+
+        @endforeach
+
+        @for($i = count($row); $i < 3; $i++)
+            <td width="33.33%"></td>
+        @endfor
+
+    </tr> --}}
+
     <tr>
-        <td width="50%">
-            <b>Hari / Tgl</b> :
-            {{ $request->date ? \Carbon\Carbon::parse($request->date)->format('d-m-Y') : 'SEMUA' }}
-        </td>
-
-        <td width="50%">
-            <b>Supplier Wire</b> :
-            {{ count($grids) ? $grids[0]['supplier'] : '-' }}
-        </td>
-    </tr>
-</table>
-
-<br>
-
-<table width="100%" border="0" cellspacing="2" cellpadding="0">
-
-    @foreach(array_chunk($rows, 3) as $row)
-
-<tr>
 
     @foreach($row as $grid)
 
@@ -209,44 +336,11 @@ $totalPage = ceil(count($grids) / 9);
             </tr>
 
             <tr>
-                <td width="35%">
-                    Tanggal
+                <td width="50%">
+                    <b>End</b>
                 </td>
-                <td width="65%">
-                    {{ \Carbon\Carbon::parse($grid['tanggal'])->format('d-m-Y') }}
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    Shift
-                </td>
-                <td>
-                    {{ $grid['shift'] }}
-                </td>
-            </tr>
-
-            {{-- <tr>
-                <td>
-                    Supplier
-                </td>
-                <td>
-                    {{ $grid['supplier'] }}
-                </td>
-            </tr> --}}
-
-            <tr>
-                <td>
-                    Produk
-                </td>
-                <td>
-                    {{ $grid['produk'] }}
-                </td>
-            </tr>
-
-            <tr>
-                <td colspan="2">
-                    <b>Detail Wire</b>
+                <td width="50%">
+                    <b>No.Lot</b>
                 </td>
             </tr>
 
@@ -255,38 +349,18 @@ $totalPage = ceil(count($grids) / 9);
                 @foreach($grid['detail'] as $dtl)
 
                 <tr>
-                    <td width="35%">
-                        <strong>Lot:</strong> 
+                    <td>
+                        {{ $dtl['end'] ?? '-' }}
                     </td>
 
-                    <td width="65%">
-                       <strong>{{ $dtl['no_lot'] ?? '' }}</strong>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td width="35%">
-                        Start - End:
-                    </td>
-
-                    <td width="65%">
-                        {{ $dtl['start'] ?? '?' }} - {{ $dtl['end'] ?? '?' }}
+                    <td>
+                        <strong>{{ $dtl['no_lot'] ?? '-' }}</strong>
                     </td>
                 </tr>
 
                 @endforeach
 
             @endif
-
-            <tr>
-                <td>
-                    Catatan
-                </td>
-
-                <td>
-                    {{ $grid['catatan'] }}
-                </td>
-            </tr>
 
         </table>
 
@@ -300,51 +374,51 @@ $totalPage = ceil(count($grids) / 9);
 
 </tr>
 
-@endforeach
+    @endforeach
 
-</table>
+    </table>
 
-<br>
+    <br>
 
-@if(!$loop->last)
-    <div style="page-break-after: always;"></div>
-@endif
+    @if(!$loop->last)
+        <div style="page-break-after: always;"></div>
+    @endif
 
-@endforeach
+    @endforeach
 
-<br><br><br>
+    <br><br><br>
 
-@php
-$ttd = $items->first();
-@endphp
+    @php
+    $ttd = $items->first();
+    @endphp
 
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-        <td width="60%"></td>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+            <td width="60%"></td>
 
-        <td width="40%">
-            <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td width="50%" align="center">
-                        Diperiksa oleh
-                        <br><br><br>
-                        ( <u>{{ $ttd->username_updated ?? $ttd->username }}</u> )
-                        <br>
-                        QC
-                    </td>
+            <td width="40%">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td width="50%" align="center">
+                            Diperiksa oleh
+                            <br><br><br>
+                            ( <u>{{ $ttd->username_updated ?? $ttd->username }}</u> )
+                            <br>
+                            QC
+                        </td>
 
-                    <td width="50%" align="center">
-                        Disetujui oleh
-                        <br><br><br>
-                        ( <u>{{ $ttd->nama_spv ?? '' }}</u> )
-                        <br>
-                        QC SPV
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+                        <td width="50%" align="center">
+                            Disetujui oleh
+                            <br><br><br>
+                            ( <u>{{ $ttd->nama_spv ?? '' }}</u> )
+                            <br>
+                            QC SPV
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 
 </body>
     
