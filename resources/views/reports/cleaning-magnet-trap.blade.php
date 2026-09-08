feef27320394fbb291432bf801b6facc83c26439
Bagus <fraber.above@gmail.com>
2026-09-08 15:42:10 +0700

    Perbaikan Form Verifikasi Magnet Trap: Judul PDF, Value Inputter, Produksi, Engineer, Pembuat

--- resources/views/reports/cleaning-magnet-trap.blade.php
+++ resources/views/reports/cleaning-magnet-trap.blade.php
@@ -98,7 +98,7 @@
     </tr>
 </table>
 
-<h2 class="title">CHECKLIST CLEANING MAGNET TRAP</h2>
+{{-- <h2 class="title">CHECKLIST CLEANING MAGNET TRAP</h2> --}}
 <br>
 <br>
 
@@ -162,15 +162,15 @@
         </td>
 
         <td class="center" style="padding:0;">
-            {{ $item->username ?? '-' }}
+            {{ optional($item->creator)->name ?? '-' }}
         </td>
 
         <td class="center" style="padding:0;">
-            {{ $item->produksi->name ?? $item->produksi_id ?? '-' }}
+            {{ optional($item->produksi)->nama_karyawan ?? '-' }}
         </td>
 
         <td class="center" style="padding:0;">
-            {{ $item->engineer->name ?? $item->engineer_id ?? '-' }}
+            {{ optional($item->engineer)->nama_karyawan ?? '-' }}
         </td>
     </tr>
 
