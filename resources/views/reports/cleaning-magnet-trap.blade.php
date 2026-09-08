cdfca089f6bdddf0a1d1c0953fd594bf64dcc91e
Bagus <fraber.above@gmail.com>
2026-09-08 15:50:25 +0700

    Rename Judul PDF Cleaning Magnet Trap

--- resources/views/reports/cleaning-magnet-trap.blade.php
+++ resources/views/reports/cleaning-magnet-trap.blade.php
@@ -51,7 +51,7 @@
 
         <td width="50%" style="border:1px solid #000;text-align:center;font-size:18px;padding:5px;">
             <b>FORM</b><br>
-            <b>VERIFIKASI MAGNET TRAP</b>
+            <b>CHECKLIST CLEANING MAGNET TRAP</b>
         </td>
 
         <td width="25%" style="border:1px solid #000;font-size:10px;padding:0;vertical-align:top;">
